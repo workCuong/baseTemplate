0d20f37c1586f847f07374196ff7faa84a2bf605
cuong tran <mailtocuong@gmail.com>
2015-06-20 19:26:57 +0300

    change structure and add angular js to bastheme

---
+++ app/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en" ng-app="app">
+<head>
+  <meta charset="utf-8">
+  <title>Home page</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="Description of website">
+  <meta name="robots" content="INDEX,FOLLOW">
+  <meta property="og:title" content="title of website">
+  <meta property="og:description" content="description of website.">
+  <link rel="shortcut icon" href="" type="image/x-icon">
+  <link rel="stylesheet" type="text/css" href="skin/css/bootstrap-theme.min.css">
+  <link rel="stylesheet" type="text/css" href="skin/css/bootstrap.min.css">
+    <!--Import script -->
+  <script type="text/javascript" src="skin/js/lib/jquery.js"></script>
+  <script type="text/javascript" src="skin/js/lib/angular.min.js"></script>
+  <script type="text/javascript" src="skin/js/lib/theme-script.js"></script>
+  <script type="text/javascript" src="skin/js/lib/bootstrap.min.js"></script>
+  <script type="text/javascript" src="skin/js/app.js"></script>
+  <!--End import script -->
+</head>
+<body>
+
+<!-- Navigation -->
+
+<!--End navigation-->
+<!--Carousel-->
+
+<!--end carousel->
+
+<!--footer-->
+<!-- <div class="container" ng-controller="testPrintController">
+  <ul>
+    <li ng-repeat="phone in phones">
+      <span>{{phone.name}}</span>
+      <p>{{phone.snippet}}</p>
+    </li>
+  </ul>
+
+</div> -->
+
+<footer>
+   
+</footer>
+<!--end footer-->
+
+</body>
+</html>
